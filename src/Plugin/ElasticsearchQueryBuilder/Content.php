5f7abfc6c8833843f6efcb39b67cd20e3748a180
Raimonds Kalnins <raimonds.kalnins@gmail.com>
2023-09-26 13:25:12 +0300

    Views support is added.

---
+++ src/Plugin/ElasticsearchQueryBuilder/Content.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\elasticsearch_helper_content\Plugin\ElasticsearchQueryBuilder;
+
+use Drupal\elasticsearch_helper_views\Plugin\ElasticsearchQueryBuilder\ElasticsearchQueryBuilderPluginBase;
+
+/**
+ * The Elasticsearch content query builder plugin class.
+ *
+ * @ElasticsearchQueryBuilder(
+ *   id = "elasticsearch_content",
+ *   label = @Translation("Content"),
+ *   description = @Translation("Query builder for indices built by Elasticsearch Helper Content module")
+ * )
+ */
+class Content extends ElasticsearchQueryBuilderPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildQuery() {
+    $query = [
+      'index' => $this->getIndices(),
+      'body' => [],
+    ];
+
+    return $query;
+  }
+
+  /**
+   * Returns a list of indices ("elasticsearch_index" filter value).
+   *
+   * @return array
+   *   The list of indices.
+   */
+  protected function getIndices() {
+    // Get filter values.
+    $values = $this->getFilterValues();
+
+    if (isset($values['elasticsearch_index'])) {
+      return $values['elasticsearch_index'];
+    }
+
+    return [];
+  }
+
+}
